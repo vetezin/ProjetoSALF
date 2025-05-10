1c3086c29fa91e1100aafa1497f5c4040d0a1c31
Gustavo Honda <hondagustavo@outlook.com>
2025-05-10 18:12:16 -0300

    teste

---
+++ ProjetoSALF/src/main/java/projetoSalf/mvc/util/Mensagem.java
@@ -0,0 +1,17 @@
+package projetoSalf.mvc.util;
+
+public class Mensagem {
+    private String mensagem;
+
+    public Mensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ ProjetoSALF/src/main/java/projetoSalf/mvc/util/SingletonDB.java
@@ -0,0 +1,18 @@
+package projetoSalf.mvc.util;
+
+public class SingletonDB {
+    private static Conexao conexao=null;
+
+    private SingletonDB() {
+    }
+
+    public static boolean conectar()
+    {
+        conexao=new Conexao();
+        return conexao.conectar("jdbc:postgresql://localhost:5432/","casofa_db","postgres","postgres123");
+    }
+    public static Conexao getConexao() {
+        return conexao;
+    }
+    //.to aqui
+}
