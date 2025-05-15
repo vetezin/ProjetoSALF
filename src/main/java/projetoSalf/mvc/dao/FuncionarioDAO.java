41abfe59cdeba16e60ea21895effd41460724bf1
vetezin <vhflorencio@live.com>
2025-05-15 16:17:40 -0300

    FuncionarioDAO concluida

--- src/main/java/projetoSalf/mvc/dao/FuncionarioDAO.java
+++ src/main/java/projetoSalf/mvc/dao/FuncionarioDAO.java
@@ -4,9 +4,12 @@ import projetoSalf.mvc.model.Funcionario;
 import projetoSalf.mvc.util.Conexao;
 import projetoSalf.mvc.util.SingletonDB;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FuncionarioDAO implements IDAO<Funcionario> {
@@ -41,74 +44,118 @@ public class FuncionarioDAO implements IDAO<Funcionario> {
     }
 
     @Override
-    public Object alterar(Funcionario entidade) {
-        return null;
+    public Funcionario alterar(Funcionario func) {
+
+        String SQL = "UPDATE funcionario SET func_nome = ? WHERE func_email = ?";
+        SingletonDB.conectar();
+        try{
+            Connection con = SingletonDB.getConexao();
+            PreparedStatement stmt = con.prepareStatement(SQL);
+
+            stmt.setString(1, func.getFuncNome());
+            stmt.setString(2, func.getFuncEmail());
+
+            if(stmt.executeUpdate() > 0){
+
+                return func;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
-    public Funcionario alterarNome(String nomeNovo, String email) {
 
+    @Override
+    public boolean apagar(Funcionario func) {
+
+        String SQL = "DELETE FROM funcionario WHERE func_email = ?";
+        SingletonDB.conectar();
+        try{
+            Connection con = SingletonDB.getConexao();
+            PreparedStatement stmt = con.prepareStatement(SQL);
+
+            stmt.setString(1, func.getFuncEmail());
+
+            if(stmt.executeUpdate() > 0){
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+    @Override
+    public Funcionario get(int id) {
+
+        String SQL = "SELECT * FROM funcionario WHERE func_cod = ?";
         Funcionario func = new Funcionario();
         SingletonDB.conectar();
-        if(nomeNovo != null && email != null || !email.contains("@")) {
-            try{
-                String SQL = "UPDATE funcionario SET func_nome = ? WHERE func_email = ?";
-                Connection con = SingletonDB.getConexao();
-                PreparedStatement stmt = con.prepareStatement(SQL);
-                stmt.setString(1, nomeNovo);
-                stmt.setString(2,email);
+        try{
 
-                if(stmt.executeUpdate() > 0){
-                    System.out.println("Funcionario alterado com sucesso!");
-                }
-                else
-                    System.out.println("Funcionario não encontrado!");
+            Connection con = SingletonDB.getConexao();
+            PreparedStatement stmt = con.prepareStatement(SQL);
+            stmt.setInt(1, id);
 
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+
+                func.setFuncNome(rs.getString("func_nome"));
+                func.setFuncCpf(rs.getString("func_cpf"));
+                func.setFuncSenha(rs.getString("func_senha"));
+                func.setFuncEmail(rs.getString("func_email"));
+                func.setFuncLogin(rs.getString("func_login"));
+                func.setFuncNivel(rs.getInt("func_nivel"));
+                return func;
+
             }
-        }else{
-            return null;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+
 
         return func;
     }
 
     @Override
-    public boolean apagar(Funcionario func) {
-        return false;
-    }
+    public List<Funcionario> get(String filtro) {
 
-
-    public boolean apagarNome( String email){
-
+        String SQL = "SELECT * FROM funcionario WHERE func_nome = ?";
 
         SingletonDB.conectar();
-        if( email != null){
-            try{
-                String SQL = "DELETE FROM funcionario WHERE func_email = ? ";
-                Connection con = SingletonDB.getConexao();
-                PreparedStatement stmt = con.prepareStatement(SQL);
+        List<Funcionario> func = new ArrayList<>();
 
-                stmt.setString(1, email);
+        try{
 
-                if(stmt.executeUpdate() > 0)
-                    return true;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            Conexao conexao = new Conexao();
+            conexao.getConnection();
+            PreparedStatement stmt = conexao.getConnection().prepareStatement(SQL);
+            stmt.setString(1, filtro);
+
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Funcionario funcionariosAdd = new Funcionario();
+                funcionariosAdd.setFuncNome(rs.getString("func_nome"));
+                funcionariosAdd.setFuncCpf(rs.getString("func_cpf"));
+                funcionariosAdd.setFuncSenha(rs.getString("func_senha"));
+                funcionariosAdd.setFuncEmail(rs.getString("func_email"));
+                funcionariosAdd.setFuncLogin(rs.getString("func_login"));
+                funcionariosAdd.setFuncNivel(rs.getInt("func_nivel"));
+                func.add(funcionariosAdd);
             }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
-        return false;
-    }
 
-    @Override
-    public Funcionario get(int id) {
-        return null;
-    }
-
-    @Override
-    public List<Funcionario> get(String filtro) {
-        return List.of();
+        return func;
     }
 }
--- src/main/java/projetoSalf/mvc/dao/TestaFuncionarioDAO.java
+++ src/main/java/projetoSalf/mvc/dao/TestaFuncionarioDAO.java
@@ -3,32 +3,48 @@ package projetoSalf.mvc.dao;
 
 import projetoSalf.mvc.model.Funcionario;
 
+import javax.xml.transform.Source;
+import java.util.List;
 import java.util.Scanner;
 
 public class TestaFuncionarioDAO {
     public static void main(String[] args) {
         Funcionario func = new Funcionario();
-        func.setFuncNome("João da Silva");
-        func.setFuncCpf("123.456.789-00");
-        func.setFuncSenha("senha123");
-        func.setFuncEmail("joao@example.com");
-        func.setFuncLogin("joaosilva");
+        func.setFuncNome("Victor");
+        func.setFuncEmail("peterson@hotmail.com");
+        func.setFuncCpf("1234567");
+        func.setFuncLogin("Peterson");
+        func.setFuncSenha("123456");
         func.setFuncNivel(1);
 
+
         FuncionarioDAO daoF = new FuncionarioDAO();
-        daoF.gravar(func);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o email: ");
-        String email = scanner.nextLine();
+//        Funcionario resultado = daoF.gravar(func);
 
 
-//        Funcionario resultado = daoF.alterarNome("jao",email);
-        boolean resultado = daoF.apagarNome(email);
+//          boolean resultado = daoF.apagar(func);
+//
+        Funcionario resultado = daoF.get(10);
 
-        if (resultado == true) {
-            System.out.println("Funcionário alterado com sucesso!");
-        } else {
-            System.out.println("Erro ao salvar funcionário.");
+        List<Funcionario> resultado2 = daoF.get("Victor");
+
+
+
+        if(!resultado2.isEmpty()){
+            System.out.println(resultado2);
         }
+//        if (resultado !=null) {
+//            System.out.println("Funcionário alterado com sucesso!");
+//            System.out.println("Nome: " + resultado.getFuncNome() + "\n" + "Email: " + resultado.getFuncEmail());
+//        } else {
+//            System.out.println("Erro ao salvar funcionário.");
+//        }
+
+
+//        if (resultado != false) {
+//            System.out.println("Funcionário alterado com sucesso!");
+//        } else {
+//            System.out.println("Erro ao salvar funcionário.");
+//        }
     }
 }
