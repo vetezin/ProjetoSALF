51b278aa228ea417bd7fcf18c9d0a07e87c40586
Gustavo Honda <hondagustavo@outlook.com>
2025-05-23 04:42:24 -0300

    Arquivo do script

---
+++ ProjetoSALF/src/main/resources/static/estoque.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+  listarEstoque();
+});
+
+
+
+async function listarEstoque() {
+  let tbody = document.querySelector("#tabelaEstoque tbody");
+  tbody.innerHTML = ""; // limpa tabela antes de preencher
+
+  try {
+    let response = await fetch("http://localhost:8080/apis/estoque");
+    let estoque = await response.json();
+
+    if (!Array.isArray(estoque)) {
+      tbody.innerHTML = `<tr><td colspan="5">${
+        estoque.mensagem || "Erro ao buscar estoque."
+      }</td></tr>`;
+      return;
+    }
+
+    if (estoque.length === 0) {
+      tbody.innerHTML = "<tr><td colspan='5'>Nenhum item de estoque encontrado.</td></tr>";
+      return;
+    }
+
+    estoque.forEach(item => {
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.produto.nome}</td>
+        <td>${item.quantidade}</td>
+        <td>${item.validade}</td>
+        <td>
+          <button class="btn btn-warning btn-sm me-1" onclick="abrirModalAcerto(${item.id}, ${item.quantidade})">Acerto</button>
+          <button class="btn btn-danger btn-sm" onclick="abrirModalSaida(${item.id}, ${item.quantidade})">Saída</button>
+        </td>
+      `;
+      tbody.appendChild(tr);
+    });
+  } catch (erro) {
+    console.error("Erro ao buscar estoque:", erro);
+    tbody.innerHTML = "<tr><td colspan='5'>Erro ao buscar estoque. Verifique a API.</td></tr>";
+  }
+}
+
+
+
+
+
+
+
+    function abrirModalAcerto(id, qtdAtual) {
+      document.getElementById("acertoEstoqueId").value = id;
+      document.getElementById("quantidadeAcerto").value = qtdAtual;
+      document.getElementById("modalAcertoEstoque").style.display = "block";
+    }
+
+    function abrirModalSaida(id, qtdAtual) {
+      document.getElementById("saidaEstoqueId").value = id;
+      document.getElementById("quantidadeSaida").max = qtdAtual;
+      document.getElementById("quantidadeSaida").value = "";
+      document.getElementById("modalSaidaProduto").style.display = "block";
+    }
+
+    function fecharModal(idModal) {
+      document.getElementById(idModal).style.display = "none";
+    }
+
+   
